198a502ed80c95177d01363a551a287a022e6798
Mashell0707 <merlsutama@gmail.com>
2023-04-03 15:02:53 +0700

    Memasukkan input dan output untuk Structure Mahasiswa

--- ActivityStructure_164/ActivityStructure2_164/ActivityStructure2_164.cpp
+++ ActivityStructure_164/ActivityStructure2_164/ActivityStructure2_164.cpp
@@ -13,3 +13,23 @@ struct Mahasiswa {
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+
+	Mahasiswa mhs;
+
+	cout << "Masukkan NIM: ";
+	cin >> mhs.nim;
+	cout << "Masukkan Nama: ";
+	cin >> mhs.nama;
+	cout << "Alamat: " << endl;
+	cout << "\tNama desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\tNama Kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Masukkan umur: ";
+	cin >> mhs.umur;
+
+
+
+}
